f3c0ce0f2cd058a3cbe3e55cb637723ee704a4bb
Muhammad Fahreza <yya77591@gmail.com>
2024-11-30 16:00:13 +0700

    updated Profile

--- mobile/app/src/main/java/com/piggybank/renote/ui/Akun/AkunViewModel.kt
+++
@@ -1,7 +0,0 @@
-package com.piggybank.renote.ui.Akun
-
-import androidx.lifecycle.ViewModel
-
-class AkunViewModel : ViewModel() {
-
-}
--- mobile/app/src/main/java/com/piggybank/renote/ui/Akun/ImageProfile.kt
+++ mobile/app/src/main/java/com/piggybank/renote/ui/Akun/ImageProfile.kt
@@ -2,14 +2,12 @@ package com.piggybank.renote.ui.Akun
 
 import android.content.Context
 import android.graphics.Canvas
-import android.graphics.Paint
 import android.graphics.Path
 import android.util.AttributeSet
 import androidx.appcompat.widget.AppCompatImageView
 
 class ImageProfile(context: Context, attrs: AttributeSet) : AppCompatImageView(context, attrs) {
 
-    private val paint = Paint()
     private val path = Path()
 
     override fun onDraw(canvas: Canvas) {
