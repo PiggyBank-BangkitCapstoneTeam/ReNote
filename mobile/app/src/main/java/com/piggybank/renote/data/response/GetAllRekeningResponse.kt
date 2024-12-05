db3b67d774a0448d8eba1b66b028f4413c032e47
Muhammad Fahreza <yya77591@gmail.com>
2024-12-05 17:55:22 +0700

    login with api success

---
+++ mobile/app/src/main/java/com/piggybank/renote/data/response/GetAllRekeningResponse.kt
@@ -0,0 +1,24 @@
+package com.piggybank.renote.data.response
+
+import com.google.gson.annotations.SerializedName
+
+data class GetAllRekeningResponse(
+
+	@field:SerializedName("data")
+	val data: List<DataItem?>? = null,
+
+	@field:SerializedName("status")
+	val status: Int? = null
+)
+
+data class DataItem(
+
+	@field:SerializedName("uang")
+	val uang: Int? = null,
+
+	@field:SerializedName("name")
+	val name: String? = null,
+
+	@field:SerializedName("id")
+	val id: String? = null
+)
